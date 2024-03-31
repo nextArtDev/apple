2c8af397bf61022fe9cc5ec0fe932b0e76c0e744
Saeed Mehmanparast <nextartisticdevelopments@gmail.com>
2024-03-31 20:37:07 +0330

    3d model

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from '@/components/Hero'
 import Highlights from '@/components/Highlights'
+import Model from '@/components/Model'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col">
       <Hero />
       <Highlights />
+      <Model />
     </main>
   )
 }
