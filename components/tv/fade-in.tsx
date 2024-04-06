cdcbf2511eb0ece68a3007de0fd3d7f614efdc63
Saeed Mehmanparast <nextartisticdevelopments@gmail.com>
2024-04-07 03:11:52 +0330

    usps

--- components/tv/fade-in.tsx
+++ components/tv/fade-in.tsx
@@ -10,6 +10,9 @@ export const FadeIn = ({ children }: Props) => {
     <motion.div
       initial={{ opacity: 0, y: 20 }}
       whileInView={{ opacity: 1, y: 0 }}
+      // margin: when it should trigger the intersectional observer, ITs likw regular margin: top right bottom left
+      // 100% beyond the top, -300px from the bottom until it stops fading in
+      //   viewport={{ margin: '-30% 0px -300px 0px' }}
       viewport={{ margin: '100% 0px -300px 0px' }}
     >
       {children}
